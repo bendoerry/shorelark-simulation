b682f17b63b026fb5cee3ff0dc5d638e451ac62e
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-18 12:13:37 +0100

    Add vision constants

--- src/animal/eye.rs
+++ src/animal/eye.rs
@@ -1,3 +1,98 @@
+use std::f32::consts::{FRAC_PI_4, PI};
+
+/// How far our eye can see:
+///
+/// -----------------
+/// |               |
+/// |               |
+/// |               |
+/// |@      %      %|
+/// |               |
+/// |               |
+/// |               |
+/// -----------------
+///
+/// If @ marks our birdie and % marks food, then a FOV_RANGE of:
+///
+/// - 0.1 = 10% of the map = bird sees no foods (at least in this case)
+/// - 0.5 = 50% of the map = bird sees one of the foods
+/// - 1.0 = 100% of the map = bird sees both foods
+const FOV_RANGE: f32 = 0.25;
+
+/// How wide our eye can see.
+///
+/// If @> marks our birdie (rotated to the right) and . marks the area
+/// our birdie sees, then a FOV_ANGLE of:
+///
+/// - PI/2 = 90° =
+///   -----------------
+///   |             /.|
+///   |           /...|
+///   |         /.....|
+///   |       @>......|
+///   |         \.....|
+///   |           \...|
+///   |             \.|
+///   -----------------
+///
+/// - PI = 180° =
+///   -----------------
+///   |       |.......|
+///   |       |.......|
+///   |       |.......|
+///   |       @>......|
+///   |       |.......|
+///   |       |.......|
+///   |       |.......|
+///   -----------------
+///
+/// - 2 * PI = 360° =
+///   -----------------
+///   |...............|
+///   |...............|
+///   |...............|
+///   |.......@>......|
+///   |...............|
+///   |...............|
+///   |...............|
+///   -----------------
+///
+/// Field of view depends on both FOV_RANGE and FOV_ANGLE:
+///
+/// - FOV_RANGE=0.4, FOV_ANGLE=PI/2:
+///   -----------------
+///   |       @       |
+///   |     /.v.\     |
+///   |   /.......\   |
+///   |   ---------   |
+///   |               |
+///   |               |
+///   |               |
+///   -----------------
+///
+/// - FOV_RANGE=0.5, FOV_ANGLE=2*PI:
+///   -----------------
+///   |               |
+///   |      ---      |
+///   |     /...\     |
+///   |    |..@..|    |
+///   |     \.../     |
+///   |      ---      |
+///   |               |
+///   -----------------
+const FOV_ANGLE: f32 = PI + FRAC_PI_4;
+
+/// How much photoreceptors there are in a single eye.
+///
+/// More cells means our birds will have more "crisp" vision, allowing
+/// them to locate the food more precisely - but the trade-off is that
+/// the evolution process will then take longer, or even fail, unable
+/// to find any solution.
+///
+/// I've found values between 3~11 sufficient, with eyes having more
+/// than ~20 photoreceptors yielding progressively worse results.
+const CELLS: usize = 9;
+
 #[derive(Debug)]
 pub struct Eye;
 
