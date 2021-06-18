b196361a2ccfd31f7034ba4bfe3f75e45a9a6f8a
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-18 20:02:06 +0100

    Add `eye` and `brain` to `Animal` struct
    This also adjusts the constructor.
    The brain is a `Network` with 3 layers.
    9->18->2

--- src/animal.rs
+++ src/animal.rs
@@ -1,5 +1,8 @@
+use lib_neural_network as nn;
 use nalgebra as na;
 use rand::Rng;
+
+use self::eye::Eye;
 
 mod eye;
 
@@ -8,14 +11,57 @@ pub struct Animal {
     crate position: na::Point2<f32>,
     crate rotation: na::Rotation2<f32>,
     crate speed: f32,
+    crate eye: Eye,
+    crate brain: nn::Network,
 }
 
 impl Animal {
     pub fn random(rng: &mut dyn rand::RngCore) -> Self {
+        let eye = Eye::default();
+
+        let brain = nn::Network::random(
+            rng,
+            &[
+                // The Input Layer
+                //
+                // Because our eye returns Vec<f32>, and our neural
+                // network works on Vec<f32>, we can pass-through
+                // numbers from eye into the neural network directly.
+                //
+                // Had our birdies had, I dunno, ears, we could do
+                // something like: `eye.cells() + ear.nerves()` etc.
+                nn::LayerTopology {
+                    neurons: eye.cells(),
+                },
+
+                // The Hidden Layer
+                //
+                // There is no best answer as to "how many neurons
+                // the hidden layer should contain" (or how many
+                // hidden layers there should be, even - there could
+                // be zero, one, two or more!).
+                //
+                // The rule of thumb is to start with a single hidden
+                // layer that has somewhat more neurons that the input
+                // layer, and see how well the network performs.
+                nn::LayerTopology {
+                    neurons: 2 * eye.cells(),
+                },
+
+                // The Output Layer
+                //
+                // Since the brain will control our bird's speed and
+                // rotation, this gives us two numbers = two neurons.
+                nn::LayerTopology { neurons: 2 },
+            ],
+        );
+
         Self {
             position: rng.gen(),
             rotation: rng.gen(),
             speed: 0.002,
+            eye,
+            brain,
         }
     }
 
